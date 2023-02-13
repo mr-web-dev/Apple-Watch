be6b3a290230963eceff29b6faba78ed31cbad8e
Webdasturchi2223 <webdasturchi2223@gmail.com>
2023-02-13 22:40:59 +0300

    commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+jQuery(document).ready(function($) {
+  var icon = $(".menu");
+  var menu = $(".mobile-slider");
+  var tl = new TimelineLite({ paused: true, reversed: true });
+  tl.fromTo(
+    ".mobile-slider",
+    0.3,
+    {
+      x: 200,
+      autoAlpha: 0
+    },
+    {
+      x: 0,
+      autoAlpha: 1,
+      ease: Power4.easeOut
+    }
+  );
+  tl.to(
+    ".filter",
+    0.3,
+    {
+      autoAlpha: 1
+    },
+    0
+  );
+  icon.click(function() {
+    tl.play();
+  });
+  $(".close-menu").click(function() {
+    tl.reverse();
+  });
+  // Also close slider when clicking outside of the menu
+  $(".filter").click(function() {
+    tl.reverse();
+  });
+});
